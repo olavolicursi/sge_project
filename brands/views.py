65b88907fb7cf4019f74f2d0ffc08d841b77bbb3
Olavo Licursi <licursiolavo@gmail.com>
2025-12-04 20:16:20 -0300

    Brands Url

--- brands/views.py
+++ brands/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from . import models
+from django.views.generic import ListView
 
-# Create your views here.
+class BrandListView(ListView):
+    model = models.Brand
+    template_name = 'brand_list.html'
+    context_object_name = 'brands'
